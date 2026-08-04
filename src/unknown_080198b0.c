346ced2d17a657fdafa1eb7fbc4eb04306bbcf40
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-04 07:19:16 -0500

    Match entity callback installers

--- src/unknown_080198b0.c
+++ src/unknown_080198b0.c
@@ -10,10 +10,28 @@ extern void FUN_080205d0(void);
 extern u32 FUN_0801cfc8(u8 value);
 extern void FUN_0801d870(u8 value);
 
+typedef void (*UnknownCallback)(void);
+
+struct UnknownEntity {
+    UnknownCallback callback;
+    const void *data;
+    u8 filler8[5];
+    u8 field13;
+    u8 field14;
+    u8 field15;
+    u8 field16;
+    u8 filler17[7];
+};
+
 extern void (*gUnknown_03002030)(void);
 extern u8 gUnknown_03001620;
 extern u8 gUnknown_030013a0;
 extern u8 gUnknown_03001b00[8];
+extern struct UnknownEntity gUnknown_03003db0[];
+
+extern void FUN_0801d618(void);
+extern void FUN_0801dfdc(void);
+extern void FUN_0801e044(void);
 
 void FUN_0801c8f0(void) {
     FUN_08012b98(60);
@@ -45,6 +63,24 @@ void FUN_0801ce70(void) {
     gUnknown_03001b00[5] = 0;
     gUnknown_03001b00[6] = 0;
     gUnknown_03001b00[7] = 0;
+}
+
+void FUN_0801eacc(u8 value) {
+    if (FUN_0801cfc8(value) == 0) {
+        gUnknown_03003db0[value].callback = FUN_0801dfdc;
+    }
+}
+
+void FUN_0801eaf8(u8 value) {
+    if (FUN_0801cfc8(value) == 0) {
+        gUnknown_03003db0[value].callback = FUN_0801e044;
+    }
+}
+
+void FUN_0801eb24(u8 value) {
+    if (FUN_0801cfc8(value) == 0) {
+        gUnknown_03003db0[value].callback = FUN_0801d618;
+    }
 }
 
 void FUN_0801eb94(u8 value) {
