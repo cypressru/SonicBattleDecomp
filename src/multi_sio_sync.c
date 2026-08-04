7a22438cae14319d188df453604e7bba7e4fbf10
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-04 02:58:41 -0500

    Match the remaining MultiSioSync support functions

--- src/multi_sio_sync.c
+++ src/multi_sio_sync.c
@@ -1,6 +1,8 @@
 #include "types.h"
 
 typedef volatile u16 vu16;
+typedef unsigned long long u64;
+typedef volatile u64 vu64;
 
 struct MultiSioSyncState {
     u8 isParent;
@@ -64,7 +66,7 @@ struct MultiSioSendState {
 extern void CpuSet(const void *, void *, u32);
 extern struct MultiSioSyncState gMultiSioSyncState;
 extern struct MultiSioSendState gMultiSioSendState;
-extern s32 FUN_08018918(void *);
+s32 FUN_08018918(void *);
 
 #define REG_SIOCNT (*(vu16 *)0x04000128)
 #define REG_TM3CNT (*(volatile u32 *)0x0400010C)
@@ -166,4 +168,163 @@ void FUN_0801886c(const void *data, u32 flag) {
 
     ((struct MultiSioPacket *)gMultiSioSyncState.sendBuffer)->checksum = ~checksum - 12;
     gMultiSioSyncState.sendReady = 1;
+}
+
+s32 FUN_08018918(void *receive) {
+    s32 checksum;
+    s32 i;
+    u16 *packet;
+    u32 j;
+    s32 receiveReady;
+    volatile u32 zero;
+
+    REG_IME = 0;
+    packet = gMultiSioSyncState.completedReceiveBuffer;
+    gMultiSioSyncState.completedReceiveBuffer = gMultiSioSyncState.nextReceiveBuffer;
+    gMultiSioSyncState.nextReceiveBuffer = packet;
+    receiveReady = gMultiSioSyncState.receiveReady;
+    gMultiSioSyncState.receiveReady = 0;
+    REG_IME = 1;
+
+    gMultiSioSyncState.receivedFlags = 0;
+    gMultiSioSyncState.status6 = 0;
+    if (receiveReady != 0) {
+        for (i = 3; i >= 0; i--) {
+            packet = (u16 *)gMultiSioSyncState.completedReceiveBuffer + i * 12;
+            checksum = 0;
+            for (j = 0; j < 10; j++)
+                checksum += packet[j];
+
+            if ((s16)checksum == -13) {
+                gMultiSioSyncState.receivedFlags |= 1 << i;
+                gMultiSioSyncState.unknown6Low |= ((struct MultiSioPacket *)packet)->flag5 << i;
+                CpuSet(packet + 2, (u8 *)receive + i * 16, 0x04000004);
+            }
+            zero = 0;
+            CpuSet((const void *)&zero, packet + 2, 0x05000004);
+        }
+    }
+
+    gMultiSioSyncState.unknown2Low |= gMultiSioSyncState.receivedFlags;
+    gMultiSioSyncState.connectedFlags |= gMultiSioSyncState.unknown2Low;
+    if (gMultiSioSyncState.receivedFlags & 1) {
+        if (gMultiSioSyncState.isParent == 8) {
+            if ((gMultiSioSyncState.receivedFlags & 3) &&
+                (gMultiSioSyncState.receivedFlags == gMultiSioSyncState.connectedFlags))
+                gMultiSioSyncState.status4 = 1;
+        } else {
+            gMultiSioSyncState.status6 = ((struct MultiSioPacket *)packet)->flag6;
+        }
+
+        if ((gMultiSioSyncState.unknown6Low & 0xE) == (gMultiSioSyncState.connectedFlags & 0xE))
+            gMultiSioSyncState.status6 = 1;
+
+        gMultiSioSyncState.status5 = ((struct MultiSioPacket *)packet)->flag4;
+    }
+
+    return gMultiSioSyncState.receivedFlags;
+}
+
+void MultiSioSyncVSync(void) {
+    void *swap;
+
+    if (gMultiSioSyncState.isParent) {
+        if (gMultiSioSyncState.sendReady && gMultiSioSyncState.stage &&
+            gMultiSioSyncState.status7) {
+            gMultiSioSyncState.receiveIndex = -1;
+            swap = gMultiSioSyncState.nextReceiveBuffer;
+            gMultiSioSyncState.nextReceiveBuffer = gMultiSioSyncState.receiveBuffer;
+            gMultiSioSyncState.receiveBuffer = swap;
+            swap = gMultiSioSyncState.nextSendBuffer;
+            gMultiSioSyncState.nextSendBuffer = gMultiSioSyncState.sendBuffer;
+            gMultiSioSyncState.sendBuffer = swap;
+            gMultiSioSyncState.sendReady = 0;
+            gMultiSioSyncState.sendIndex = 0;
+            gMultiSioSyncState.errorFlags = ((volatile struct SioMultiCnt *)0x04000128)->error;
+            ((struct SioMultiCnt *)0x04000128)->data = 0xFEFE;
+            REG_SIOCNT |= 0x80;
+            *(vu16 *)0x0400010E = 0xC0;
+        }
+    } else {
+        if (gMultiSioSyncState.sioInterrupted <= 3) {
+            gMultiSioSyncState.sioInterrupted++;
+        } else {
+            REG_IME = 0;
+            *(vu16 *)0x03007FF8 |= 0x80;
+            REG_IME = 1;
+        }
+    }
+}
+
+void MultiSioSyncInterrupt(void) {
+    s32 i;
+    void *swap;
+    u16 received[4];
+
+    *(u64 *)received = *(vu64 *)0x04000120;
+    gMultiSioSyncState.errorFlags = ((volatile struct SioMultiCnt *)0x04000128)->error;
+
+    if ((received[0] == 0xFEFE) && (gMultiSioSyncState.receiveIndex > 9)) {
+        gMultiSioSyncState.receiveIndex = -1;
+        swap = gMultiSioSyncState.nextReceiveBuffer;
+        gMultiSioSyncState.nextReceiveBuffer = gMultiSioSyncState.receiveBuffer;
+        gMultiSioSyncState.receiveBuffer = swap;
+
+        if (gMultiSioSyncState.sendReady) {
+            swap = gMultiSioSyncState.nextSendBuffer;
+            gMultiSioSyncState.nextSendBuffer = gMultiSioSyncState.sendBuffer;
+            gMultiSioSyncState.sendBuffer = swap;
+            gMultiSioSyncState.sendReady = 0;
+            gMultiSioSyncState.sendIndex = 0;
+        }
+
+        REG_IME = 0;
+        *(vu16 *)0x03007FF8 |= 0x80;
+        REG_IME = 1;
+    }
+
+    if (gMultiSioSyncState.sendIndex <= 9)
+        ((struct SioMultiCnt *)0x04000128)->data =
+            ((u16 *)gMultiSioSyncState.nextSendBuffer)[gMultiSioSyncState.sendIndex];
+
+    if (gMultiSioSyncState.sendIndex <= 10)
+        gMultiSioSyncState.sendIndex++;
+
+    if (gMultiSioSyncState.receiveIndex >= 0) {
+        for (i = 0; i < 4; i++)
+            ((u16(*)[12])gMultiSioSyncState.receiveBuffer)[i][gMultiSioSyncState.receiveIndex] =
+                received[i];
+
+        if (gMultiSioSyncState.receiveIndex == 9)
+            gMultiSioSyncState.receiveReady = 1;
+    }
+
+    if (gMultiSioSyncState.receiveIndex <= 10)
+        gMultiSioSyncState.receiveIndex++;
+
+    if (gMultiSioSyncState.isParent)
+        *(vu16 *)0x0400010E = 0;
+
+    if ((gMultiSioSyncState.sendIndex <= 10) && gMultiSioSyncState.isParent) {
+        REG_SIOCNT |= 0x80;
+        *(vu16 *)0x0400010E = 0xC0;
+    }
+
+    gMultiSioSyncState.sioInterrupted = 0;
+}
+
+void FUN_08018c20(void) {
+    if (gMultiSioSyncState.isParent)
+        gMultiSioSyncState.status7 = 1;
+}
+
+void FUN_08018c3c(void) {
+    REG_IME = 0;
+    REG_IE &= 0xFF3F;
+    REG_IME = 1;
+
+    REG_SIOCNT = 0x2003;
+    REG_TM3CNT = 0xABFB;
+    REG_IF = 0xC0;
+    gMultiSioSyncState.status7 = 0;
 }
