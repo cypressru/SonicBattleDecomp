dd63a72dff8bffbe299045aac58aa1922fa210b6
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-04 15:02:49 -0500

    Match multiplayer render object initializers

--- src/unknown_080198b0.c
+++ src/unknown_080198b0.c
@@ -444,6 +444,11 @@ extern const u16 gUnknown_08edbb9c[][2];
 extern const u8 gUnknown_0811c0e0[];
 extern const u16 gUnknown_08edbbac[][2];
 extern void FUN_0802b688(struct UnknownListNode *node);
+extern void FUN_0802b248(struct UnknownListNode *node);
+extern void FUN_0802b388(struct UnknownListNode *node);
+extern const u8 gUnknown_0811c120[];
+extern const u8 gUnknown_0811c188[];
+extern const u16 gUnknown_08edbb7c[][2];
 extern u32 gUnknown_03005264;
 extern u8 gUnknown_03002600[];
 extern u8 gUnknown_03005268[];
@@ -5179,4 +5184,28 @@ void FUN_0802b688(struct UnknownListNode *node) {
         }
         FUN_0801fed8(current->field6, 0);
     }
+}
+
+void FUN_0802b2d4(struct UnknownListNode *node) {
+    node->position->field0 = gUnknown_0811c120;
+    node->position->tile = 768;
+    node->position->x = 76;
+    node->position->y = 0;
+    node->position->field10 = 0;
+    node->position->field11 = 0;
+    node->position->field12 = 0;
+    node->position->field13 = 0xFF;
+    node->position->field14 = 0xFF;
+    node->data = (const void *)((u32)FUN_0802b248 + 1);
+}
+
+void FUN_0802b31c(struct UnknownListNode *node) {
+    node->position->field0 = gUnknown_0811c188;
+    node->position->tile = node->position->field13 * 80 + 448;
+    node->position->x = gUnknown_08edbb7c[node->position->field13][0];
+    node->position->y = gUnknown_08edbb7c[node->position->field13][1];
+    node->position->field10 = 0;
+    node->position->field11 = 0;
+    node->position->field12 = (gUnknown_0300524c[node->position->field13] << 4) | 4;
+    node->data = (const void *)((u32)FUN_0802b388 + 1);
 }
