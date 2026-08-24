06c4c3e7e00699c05b40045d1922ddd933b2ad6c
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-24 11:00:03 -0500

    Preserve script conversion state reads

--- src/unknown_080460ac.c
+++ src/unknown_080460ac.c
@@ -614,19 +614,16 @@ void FUN_08046bc4(struct UnknownState460ac *state) {
                     done = 0;
                     break;
                 case 0xfff9: {
-                    s32 conversionResult;
-
                     command = nextScript[0];
                     state->field28.script = nextScript + 1;
-                    conversionResult =
+                    gUnknown_03000285 =
                         FUN_080406d4(gUnknown_03000274[command], (s8 *)gUnknown_03000280, 5);
-                    gUnknown_03000285 = conversionResult;
-                    if (conversionResult < 0) {
+                    if (gUnknown_03000285 < 0) {
                         gUnknown_03000285 = 0;
                     }
                     FUN_08046af8(state);
                     state->field27 = 4;
-                    if (conversionResult > 4) {
+                    if (gUnknown_03000285 > 4) {
                         state->field27 = 1;
                     }
                     break;
