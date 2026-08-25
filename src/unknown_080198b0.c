59c916739b8c2303d3eaf0a117436fb73e94efdd
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-25 07:10:00 -0500

    Relax fallback scratch bindings

--- src/unknown_080198b0.c
+++ src/unknown_080198b0.c
@@ -7650,7 +7650,7 @@ void FUN_08029250(void) {
                         do {
                             u8 column;
                             register u32 scaledGroup asm("r1") = firstGroup;
-                            register u32 scratch asm("r0");
+                            register u32 scratch;
                             register u8 *destination asm("r6");
                             register u8 *destinationAddress asm("r2");
                             register const u16 *entries asm("r5");
@@ -7706,7 +7706,7 @@ void FUN_08029250(void) {
                         do {
                             u8 column;
                             register u32 scaledGroup asm("r1") = secondGroup;
-                            register u32 scratch asm("r0");
+                            register u32 scratch;
                             register u8 *destination asm("r6");
                             register u8 *destinationAddress asm("r2");
                             register const u16 *entries asm("r5");
@@ -7762,7 +7762,7 @@ void FUN_08029250(void) {
                         do {
                             u8 column;
                             register u32 scaledGroup asm("r1") = thirdGroup;
-                            register u32 scratch asm("r0");
+                            register u32 scratch;
                             register u8 *destination asm("r6");
                             register u8 *destinationAddress asm("r2");
                             register const u16 *entries asm("r5");
@@ -7823,7 +7823,7 @@ void FUN_08029250(void) {
                         do {
                             u8 column;
                             register u32 scaledGroup asm("r1") = fourthGroup;
-                            register u32 scratch asm("r0");
+                            register u32 scratch;
                             register u8 *destination asm("r6");
                             register const u16 *entries asm("r5");
 
