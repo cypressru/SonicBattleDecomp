39659deafa53da2a4261f77bf7d0126bf2832ef6
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-25 06:01:09 -0500

    Remove redundant graphics macro constraints

--- src/unknown_080198b0.c
+++ src/unknown_080198b0.c
@@ -8874,7 +8874,6 @@ void FUN_08029250(void) {
 #define LOAD_PLAYER_GRAPHICS(index, destination)                                                   \
     do {                                                                                           \
         register u32 value asm("r0") = gUnknown_0300524c[index];                                   \
-        asm volatile("" : "+r"(value));                                                            \
         if (value > 9) {                                                                           \
             value = 10;                                                                            \
         } else if (value > 8) {                                                                    \
@@ -8882,7 +8881,6 @@ void FUN_08029250(void) {
         }                                                                                          \
         {                                                                                          \
             register u32 source asm("r1") = (u32)gUnknown_08edbadc[value];                         \
-            asm volatile("" : "+r"(source));                                                       \
             FUN_08020408((destination), source);                                                   \
         }                                                                                          \
     } while (0)
