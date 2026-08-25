805d81f0172d93100d0215f04201fd383b71136b
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-25 06:05:13 -0500

    Remove redundant high bit constraint

--- src/unknown_080198b0.c
+++ src/unknown_080198b0.c
@@ -8482,7 +8482,6 @@ void FUN_08029d74(struct UnknownListNode *node) {
                     register u32 highBit asm("r4") = 128;
                     register u32 bit asm("r0");
 
-                    asm volatile("" : "+r"(highBit));
                     highBit <<= 1;
                     asm volatile("" : "+r"(highBit));
                     bit = highBit;
